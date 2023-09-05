c97c67b0a4578d5d9033cd97937984a56a7449f4
dti <dti@LAB204-01>
2023-09-05 09:18:35 -0300

    refatoração

--- produto/src/main/java/com/fatec/produto/ProdutoApplication.java
+++ produto/src/main/java/com/fatec/produto/ProdutoApplication.java
@@ -2,7 +2,11 @@ package com.fatec.produto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * 
+ * @author Jennifer
+ * 
+ */
 @SpringBootApplication
 public class ProdutoApplication {
 
