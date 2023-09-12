4daae3579f4d77209caff2edf45e4ef47fbd4a28
jennifer cristine <107707471+jennifer-cristine12@users.noreply.github.com>
2023-09-12 11:29:39 -0300

    Add files via upload

---
+++ produto/src/test/java/com/fatec/produto/Spring/produto/src/main/java/com/fatec/produto/servico/ProdutoServico.java
@@ -0,0 +1,37 @@
+package com.fatec.produto.servico;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.fatec.produto.model.Imagem;
+import com.fatec.produto.model.Catalogo;
+import com.fatec.produto.model.IProdutoRepository;
+import com.fatec.produto.model.Produto;
+
+@Service
+public class ProdutoServico implements IProdutoServico {
+	@Autowired
+	IProdutoRepository repositoryP;
+	@Autowired
+	ImagemServico imagemServico;
+
+	public List<Catalogo> consultaCatalogo() {
+
+		Catalogo c = null;
+		List<Catalogo> lista = new ArrayList<Catalogo>();
+		List<Produto> listaP = repositoryP.findAll();
+		List<Imagem> listaI = imagemServico.getAll();
+		for (Produto p : listaP) {
+			for (Imagem i : listaI) {
+				if (p.getId().equals(i.getId())) {
+					c = new Catalogo(p.getId(), p.getDescricao(), p.getCategoria(), p.getCusto(),
+							p.getQuantidadeNoEstoque(), i.getArquivo());
+					lista.add(c);
+				}
+			}
+		}
+		return lista;
+	}
+}
---
+++ produto/src/test/java/com/fatec/produto/Spring/produto/src/test/java/com/fatec/produto/Req02ConsultarCatalogoTest.java
@@ -0,0 +1,22 @@
+package com.fatec.produto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fatec.produto.model.Produto;
+import com.fatec.produto.servico.IProdutoServico;
+@SpringBootTest
+class Req02ConsultarCatalogoTest {
+	@Autowired
+	IProdutoServico produtoServico;
+	@Test
+	void test() {
+		
+	}
+
+}
