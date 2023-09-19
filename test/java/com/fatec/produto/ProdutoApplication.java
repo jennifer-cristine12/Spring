0ccca1be8cca8caf41537330ee4432fd674e7779
jennifer cristine <107707471+jennifer-cristine12@users.noreply.github.com>
2023-09-19 11:20:22 -0300

    Add files via upload

---
+++ test/java/com/fatec/produto/ProdutoApplication.java
@@ -0,0 +1,17 @@
+package com.fatec.produto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+ * 
+ * @author Jennifer
+ * 
+ */
+@SpringBootApplication
+public class ProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoApplication.class, args);
+	}
+
+}
